9f339ebe847ffcabb1901cf7ac8123ae9da76a89
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-11-22 17:46:06 -0300

    adicionando action ao redux

--- src/pages/FruitsPage/FruitsPage.js
+++ src/pages/FruitsPage/FruitsPage.js
@@ -1,18 +1,34 @@
-//importando useSelector do react-redux
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFruit } from "../../store/modules/fruits/actions";
 import { Container } from "./style";
 
 const FruitsPage = () => {
-  //acessando o estado global e pegando o state fruits
   const fruits = useSelector((state) => state.fruits);
+  const dispatch = useDispatch();
+  const [fruit, setFruit] = useState("");
+
+  const handleAddFruit = () => {
+    dispatch(addFruit(fruit));
+    setFruit("");
+  };
 
   return (
     <Container>
-      {fruits.map((fruit) => (
-        <div>
-          <p key={fruit}> {fruit} </p>
-        </div>
-      ))}
+      <input
+        placeholder="Fruta"
+        value={fruit}
+        onChange={(evt) => setFruit(evt.target.value)}
+      />
+      <button onClick={handleAddFruit}>Adicionar Fruta</button>
+
+      <section>
+        {fruits.map((fruit) => (
+          <div>
+            <p key={fruit}> {fruit} </p>
+          </div>
+        ))}
+      </section>
     </Container>
   );
 };
--- src/pages/FruitsPage/style.js
+++ src/pages/FruitsPage/style.js
@@ -11,12 +11,36 @@ export const Container = styled.div`
   border: 2px goldenrod solid;
   border-radius: 8px;
 
-  > div {
-    border: 2px solid purple;
-    width: 100px;
-    background-color: #ffb646;
-    color: black;
-    font-size: 20px;
+  input {
+    height: 20px;
     border-radius: 8px;
+    text-align: center;
+  }
+
+  button {
+    border: 0;
+    background: #f3cb78;
+    border-radius: 8px;
+    cursor: pointer;
+    margin: 4px;
+
+    :hover {
+      transform: scale(1.1);
+      transition: 0.5s;
+    }
+  }
+
+  > section {
+    overflow-y: scroll;
+
+    div {
+      margin: 10px;
+      border: 2px solid purple;
+      width: 100px;
+      background-color: #ffb646;
+      color: black;
+      font-size: 20px;
+      border-radius: 8px;
+    }
   }
 `;
